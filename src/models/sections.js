dd819f5bd860340b1c5842165e4ea0a9a7d043dc
Samer985 <63476785+Samer985@users.noreply.github.com>
2025-12-12 23:07:00 +0300

    Create sections.js

---
+++ src/models/sections.js
@@ -0,0 +1,7 @@
+```javascript
+export const IPE = {
+'IPE100': { h: 100, b: 55, tw: 4.1, tf: 5.7 },
+'IPE200': { h: 200, b: 100, tw: 5.6, tf: 8.5 },
+'IPE300': { h: 300, b: 150, tw: 6.9, tf: 10.2 }
+};
+```
